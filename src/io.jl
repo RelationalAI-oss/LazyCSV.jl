5563bbc7f2c9421013861d9d67f1f4398f918961
Mohammad Dashti <mdashti@gmail.com>
2019-03-19 20:35:16 +0330

    Added support for reading the data from a URL

--- src/io.jl
+++ src/io.jl
@@ -1,9 +1,16 @@
+using HTTP
+
 function read_input_data(input_source::AbstractString; kwargs...)
     #TODO this should be done async
-    open(input_source, "r")
+    if startswith(input_source, "https://") || startswith(input_source, "http://")
+        HTTP.get(input_source; kwargs...)
+    else
+        open(input_source, "r")
+    end
 end
 read_input_data(input::IOStream; kwargs...) = input
 
+apply_mmap(input::HTTP.Messages.Response) = input.body
 apply_mmap(input) = Mmap.mmap(input)
 
 function read_mmap_data(input_source::Union{AbstractString,IOStream}; kwargs...)
